766ce21e266ac631199b9dab7a42b1381f98f47a
slsevilla <slsevilla@gmail.com>
2025-01-20 17:09:34 +0000

    chore: update post pipeline results

--- bin/post_process.sh
+++ bin/post_process.sh
@@ -1,126 +1,104 @@
-# bash bin/core_wgs_id.sh /home/ubuntu/output/OH-VH00648-230526/pipeline/batch_1 OH-VH00648-230526
+# bash bin/core_wgs_id.sh phoenix_results.sh labResults.csv
+
 #########################################################
 # ARGS
 #########################################################
-# output_dir=$1
-# project_name_full=$2
-# wgs_results=$3
-# pipeline_results=$4
-# pipeline_log=$5
-
 core_functions=$1
-phoenix_results=$2
+rawPipeline_results=$2
+lab_results=$3
 
 ##########################################################
 # Eval, source
 #########################################################
 source $(dirname "$0")/$core_functions
 
-# ##########################################################
-# # Set files, dir
-# #########################################################
-# wgs_dir="/home/ubuntu/workflows/AST_Workflow/wgs_db"
+##########################################################
+# Set files, dir
+#########################################################
+intermed_results="pipeline_results.csv"
+processed_samples="processed_samples.csv"
+quality_results="quality_results.csv"
 
-# #########################################################
-# # project variables
-# #########################################################
-# # set project shorthand
-# project_name=$(echo $project_name_full | cut -f1 -d "_" | cut -f1 -d " ")
+#########################################################
+# project variables
+#########################################################
 
 ##########################################################
 # Run code
 #########################################################
-# convert format
-cp $phoenix_results pipeline_results.csv
-sed -i "s/\t/;/g" pipeline_results.csv
+# pull sample files
+cat $rawPipeline_results | awk -F"\t" '{print $1}' | grep -v "ID" | uniq > $processed_samples
+IFS=$'\n' read -d '' -r -a sample_list < $processed_samples
 
-# review synopsis and determine status
-cat pipeline_results.csv | awk -F";" '{print $1}' | grep -v "ID" | uniq > processed_samples.csv
-IFS=$'\n' read -d '' -r -a sample_list < processed_samples.csv
-
+# for each sample
+## update the synopsis file
+## review lab results
+## update pass/fail
 for sample_id in "${sample_list[@]}"; do
-	# process all samples
-	echo $sample_id
-		
-	# pull only ID
-	clean_id=$(clean_file_names $sample_id)
-
 	# pull stats
 	cat ${sample_id}_trimmed_read_counts.txt >> all_trimmed_read_counts.txt #stats.txt
 
 	# determine number of warnings, fails
-	synopsis=$log_dir/pipeline/$sample_id.synopsis
+	synopsis=$sample_id.synopsis
 	num_of_warnings=`cat $synopsis | grep -v "WARNINGS" | grep "WARNING" | wc -l`
 	num_of_fails=`cat $synopsis | grep -v "completed as FAILED" | grep "FAILED" | wc -l`
 
-		# # review lab results
-		# labValue=`cat $lab_results | grep $sample_id | cut -f2 -d";" | sort | awk '{print $1}'`
-		# pipelineValue=`cat $phoenix_results | grep $sample_id | awk -F"\t" '{print $9}' | sort | awk '{print $1}'`
-		# pipelineStatus=`cat $phoenix_results | grep $sample_id | awk -F"\t" '{print $2}'`
+	# review lab results
+	labValue=`cat $lab_results | grep $sample_id | cut -f2 -d"," | sort | awk '{print $1}'`
+	pipelineValue=`cat $rawPipeline_results | grep $sample_id | awk -F"\t" '{print $9}' | sort | awk '{print $1}'`
+	pipelineStatus=`cat $rawPipeline_results | grep $sample_id | awk -F"\t" '{print $2}'`
 		
-		# # message if the lab didnt give results
-		# if [[ $labValue == "" ]]; then echo "Missing lab value: $sample_id"; fi
+	# message if the lab didnt give results
+	if [[ $labValue == "" ]]; then echo "Missing lab value: $sample_id" >> $log_results; fi
 
-		# # update the results and reasons
-		# SID=$(awk -F"\t" -v sid=$sample_id '{ if ($1 == sid) print NR }' $phoenix_results)
-		# if [[ $num_of_warnings -gt 4 ]]; then
-		# 	echo "--fail: exceeds warnings"
-		# 	reason=$(cat $synopsis | grep -v "Summarized" | grep -E "WARNING|FAIL" | awk -F": " '{print $3}' |  awk 'BEGIN { ORS = "; " } { print }' | sed "s/; ; //g")
-		# 	cat pipeline_results.csv | awk -F";" -v i=$SID -v reason="${reason}" 'BEGIN {OFS = FS} NR==i {$2="FAIL"; $24=reason}1' > tmp; mv tmp pipeline_results.csv
-		# else
-		# 	if [[ $pipelineStatus == "PASS" && *"$pipelineValue" != *"$labValue"*  ]]; then
-		# 		reason="Lab Discordance"
-		# 		cat pipeline_results.csv | awk -F";" -v i=$SID -v reason="${reason}" 'BEGIN {OFS = FS} NR==i {$2="FAIL"; $24=reason}1' > tmp; mv tmp pipeline_results.csv
-		# 		echo "***"
-		# 		echo "FAILED: discordance found $pipelineValue versus $labValue"
-		# 		echo "***"
-		# 	else
-		# 		echo "--pass"
-		# 	fi
-		# fi
+	# update the results and reasons
+	SID=$(awk -F"\t" -v sid=$sample_id '{ if ($1 == sid) print NR }' $rawPipeline_results)
+	if [[ $num_of_fails -gt 0 ]]; then
+		reason=$(cat $synopsis | grep -v "completed as" | grep -E "FAILED" | awk -F": " '{print $3}' |  awk 'BEGIN { ORS = "; " } { print }' | sed "s/; ; //g")
+		cat $rawPipeline_results | awk -F";" -v i=$SID -v reason="${reason}" 'BEGIN {OFS = FS} NR==i {$2="FAIL"; $24=reason}1' > tmp
+		mv tmp $rawPipeline_results
 
-		# # set MLST scheme
-		# species=`cat pipeline_results.csv | grep $sample_id | awk -F";" '{print $9}' | sort | uniq`
-        # MLST_1=`cat pipeline_results.csv | grep $sample_id | awk -F";" '{print $16}' | sort | uniq | cut -f1 -d","`
-        # MLST_Scheme_1=`cat pipeline_results.csv | grep $sample_id | awk -F";" '{print $15}' | sort | uniq`
-        # MLST_2=`cat pipeline_results.csv | grep $sample_id | awk -F";" '{print $18}'| sort | uniq | cut -f1 -d","`
-        # MLST_Scheme_2=`cat pipeline_results.csv | grep $sample_id | awk -F";" '{print $17}'| sort | uniq`
+		echo "$sample_id,FAIL,FAILED" >> $quality_results
+	elif [[ $num_of_warnings -gt 4 ]]; then
+		reason=$(cat $synopsis | grep -v "Summarized" | grep -E "WARNING" | awk -F": " '{print $3}' |  awk 'BEGIN { ORS = "; " } { print }' | sed "s/; ; //g")
+		cat $rawPipeline_results | awk -F";" -v i=$SID -v reason="${reason}" 'BEGIN {OFS = FS} NR==i {$2="FAIL"; $24=reason}1' > tmp
+		mv tmp $rawPipeline_results
 
-		# # handle schemes that have parenthesis
-        # if [[ $MLST_Scheme_1 =~ "(" ]]; then MLST_Scheme_1=`echo $MLST_Scheme_1 | sed -E -n 's/.*\((.*)\).*$/\1/p'`; fi
-        # if [[ $MLST_Scheme_2 =~ "(" ]]; then MLST_Scheme_2=`echo $MLST_Scheme_2 | sed -E -n 's/.*\((.*)\).*$/\1/p'`; fi
+		echo "$sample_id,FAIL,WARNING" >> $quality_results
+	else
+		if [[ $pipelineStatus == "PASS" && *"$pipelineValue" != *"$labValue"*  ]]; then
+			reason="Lab Discordance"
+			cat $rawPipeline_results | awk -F"\t" -v i=$SID -v reason="${reason}" 'BEGIN {OFS = FS} NR==i {$2="FAIL"; $24=reason}1' > tmp
+			mv tmp $rawPipeline_results
+			echo "$sample_id,FAIL,labDiscordance($pipelineValue,$labValue)" >> $quality_results
+		else
+			echo "$sample_id,PASS,NA" >> $quality_results
+		fi
+	fi
 
-        # # check if the first scheme exists
-		# if [[ $MLST_1 == "-" ]] || [[ $MLST_1 == *"Novel"* ]]; then
-        #     sequence_classification="MLST__${species}"
-        # else
-		# 	# check if there is a second MLST
-		# 	if [[ $MLST_2 == "-" ]]; then
-		# 		sequence_classification=`echo "ML${MLST_1}_${MLST_Scheme_1}_${species}"`
-		# 	else
-		# 		sequence_classification=`echo "ML${MLST_1}_${MLST_Scheme_1}_${species}-ML${MLST_2}_${MLST_Scheme_2}_${species}"`
-		# 	fi
-        # fi
+	# set MLST scheme
+	species=`cat $rawPipeline_results | grep $sample_id | awk -F"\t" '{print $9}' | sort | uniq`
+    MLST_1=`cat $rawPipeline_results | grep $sample_id | awk -F"\t" '{print $16}' | sort | uniq | cut -f1 -d","`
+    MLST_Scheme_1=`cat $rawPipeline_results | grep $sample_id | awk -F"\t" '{print $15}' | sort | uniq`
+    MLST_2=`cat $rawPipeline_results | grep $sample_id | awk -F"\t" '{print $18}'| sort | uniq | cut -f1 -d","`
+    MLST_Scheme_2=`cat $rawPipeline_results | grep $sample_id | awk -F"\t" '{print $17}'| sort | uniq`
+
+	# handle schemes that have parenthesis
+    if [[ $MLST_Scheme_1 =~ "(" ]]; then MLST_Scheme_1=`echo $MLST_Scheme_1 | sed -E -n 's/.*\((.*)\).*$/\1/p'`; fi
+    if [[ $MLST_Scheme_2 =~ "(" ]]; then MLST_Scheme_2=`echo $MLST_Scheme_2 | sed -E -n 's/.*\((.*)\).*$/\1/p'`; fi
+
+    # check if the first scheme exists
+	if [[ $MLST_1 == "-" ]] || [[ $MLST_1 == *"Novel"* ]]; then
+        sequence_classification="MLST__${species}"
+    else
+		# check if there is a second MLST
+		if [[ $MLST_2 == "-" ]]; then
+			sequence_classification=`echo "ML${MLST_1}_${MLST_Scheme_1}_${species}"`
+		else
+			sequence_classification=`echo "ML${MLST_1}_${MLST_Scheme_1}_${species}-ML${MLST_2}_${MLST_Scheme_2}_${species}"`
+		fi
+    fi
 		
-		# # Add MLST
-		# awk -v add="$sequence_classification;$labValue" -v sample="$sample_id" '$0 ~ sample {print $0";"add}' "pipeline_results.csv" >> "$mlst_file"
+	# Add corrected MLST and labvalue
+	awk -v add="$sequence_classification\t$labValue" -v sample="$sample_id" '$0 ~ sample {print $0";"add}' "$rawPipeline_results" >> "$intermed_results"
 done
-
-
-
-
-
-
-# # read in final report; create sample list
-# IFS=$'\n' read -d '' -r -a sample_list < $phoenix_results
-
-# 	# create copy of results
-# 	cd $pipeline_dir
-# 	mlst_file=mlst_output.csv
-# 	if [[ -f $mlst_file ]]; then rm $mlst_file; fi
-
-	
-	
-
-# 	# cleanup
-# 	cat $mlst_file | sort | uniq > pipeline_results.csv
--- modules/local/post_process.nf
+++ modules/local/post_process.nf
@@ -2,42 +2,26 @@ process POST_PROCESS {
     label 'process_single'
 
     input:
-    path(trimmed_read_counts_files_ch)
+    path(all_files_ch)
     path(final_phoenix_summary)
     path(core_functions_script)
-    // path(phoenix_results)
+    path(labResults)
 
     output:
     path('pipeline_results.csv')         , emit: pipeline_results
-    path('processed_samples.csv')        , emit: processed_samples
-    path("versions.yml")                , emit: versions
+    path('quality_results.csv')           , emit: quality_results
+    path("versions.yml")                 , emit: versions
 
-    script: // This script is bundled with the pipeline, in cdcgov/phoenix/bin/
-    // define variables
+    script:
     """
     post_process.sh \
         $core_functions_script \
-        $final_phoenix_summary
+        $final_phoenix_summary \
+        $labResults
+
+    cat <<-END_VERSIONS > versions.yml
+    "${task.process}":
+        post_process_tag: "v1.0"
+    END_VERSIONS
     """
 }
-
-    //     output_dir=$1
-    //     project_name_full=$2
-    //     wgs_results=$3
-    //     pipeline_results=$4
-    //     pipeline_log=$5
-
-    // create_phoenix_summary_tsv.py \\
-    //     --out Phoenix_Summary.tsv \\
-    //     $busco_parameter
-
-    // cat <<-END_VERSIONS > versions.yml
-    // "${task.process}":
-    //     python: \$(python --version | sed 's/Python //g')
-    //     phoenix_base_container_tag: ${container_version}
-    // END_VERSIONS
-
-// # create a pass list
-// awk '{print $1","$2}' $phoenix_results | grep - "FAIL"
-// 24AR003724-OH-VH00648-240920,FAIL
-// 24AR004033-OH-VH00648-241004,FAIL
