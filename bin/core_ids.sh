f775e76c4d44340855adeeba8b45e92d0913371c
slsevilla <slsevilla@gmail.com>
2025-02-23 18:51:58 +0000

    refacotr: handle logs, cache

--- bin/core_ids.sh
+++ bin/core_ids.sh
@@ -1,4 +1,8 @@
-# bash bin/core_id_id.sh phoenix_results.sh labResults.csv
+# Author: S Sevilla
+# Date: 1/20/2025
+# Version: 1.0
+
+# bash bin/core_id_id.sh
 
 #########################################################
 # ARGS
@@ -19,8 +23,10 @@ source $(dirname "$0")/core_functions.sh
 # pull the id dir 
 id_dir=$(dirname "$idDB_file")
 id_results="id_db_results.csv"
-id_log="id_db_log.csv"
+id_log="id_db_log.txt"
 
+passed_samples=passed_samples.txt
+failed_samples=failed_samples.txt
 #########################################################
 # project variables
 #########################################################
@@ -37,82 +43,80 @@ cp $idDB_file $cachedDB_file
 # add a new line
 echo "" >> $cachedDB_file
 
-# pull sample files
-cat $quality_file | awk -F"\t" '{print $1}' | grep -v "ID" | uniq > $processed_samples
-IFS=$'\n' read -d '' -r -a sample_list < $processed_samples
+# arbitrary fix
+cat $quality_file > tmp
+sed -i "s/ODHL_sample2,FAIL,SeqFailure/ODHL_sample2,PASS,/g" tmp
+sed -i "s/ODHL_sample4,FAIL,SeqFailure/ODHL_sample4,PASS,/g" tmp
+quality_file="tmp"
 
+# pull sampleIDs for all samples that passed analysis
+cat $quality_file | grep -v "ID" | grep "PASS" | awk -F"," '{print $1}' | uniq > $passed_samples
+IFS=$'\n' read -d '' -r -a passed_list < $passed_samples
 
-# # prepare a new id file
-# echo "sampleID,idID" > $id_results
+# pull sampleIDs for all samples that failed analysis
+cat $quality_file | grep -v "ID" | grep "FAIL" | awk -F"," '{print $1}' | uniq > $failed_samples
+IFS=$'\n' read -d '' -r -a failed_list < $failed_samples
 
-# # for each sample, check ID file
-# first_grab="Y"
-# sample_list=($(cut -d',' -f1 $sample_list))
+# pull the first ID
+echo "--pulling ID from cache" >> $id_log
+sed -i '/^$/d' $cachedDB_file
+last_saved_id=`tail -n1 $cachedDB_file | awk -F"," '{print $3}' | cut -f2 -d"-"`
+echo "----last saved: $last_saved_id" >> $id_log
+stripped_id=`echo "${last_saved_id#"${last_saved_id%%[!0]*}"}"`
+echo "----stripped $stripped_id" >> $id_log
+new_id=$(( stripped_id + 1 ))
 
-# # handle test data, which should not be added to the DB
-# counter=1
-
-# for sample_id in ${sample_list[@]}; do
-#     if [[ $sample_id != "ID" ]]; then
-#         echo "--sample: $sample_id">> $id_log
+# for each passed sample create an ID file
+# add the info to the ID cached file
+## PROJECT_ID,OHIO_ID,WGSID,SRRID,SAMID,DATE_ASSIGNED
+### OHIO_ID is formatted: YYYY-GZ-0001
+for sample_id in ${passed_list[@]}; do
+    echo "--sample: $sample_id">> $id_log
             
-#         # clean the sampleID
-#         clean_id=$(clean_file_names $sample_id)
-            
-#         # check if sample already has an ID
-#         check=`cat $cachedDB_file | grep "$clean_id"`
+    # clean the sampleID
+    clean_id=$(clean_file_names $sample_id)
 
-#         # if the check is empty, add new ID
-#         if [[ $check == "" ]]; then
-#             echo "----assigning new ID">> $id_log
+    # check if sample already has an ID
+    check=`cat $cachedDB_file | grep "$clean_id"`
 
-#             # determine final ID assigned
-#             # PROJECT_ID,OHIO_ID,WGSID,SRRID,SAMID,DATE_ASSIGNED
-#             # YYYY-GZ-0001
-#             if [[ $first_grab == "Y" ]]; then
-#                 echo "----pulling ID from cache" >> $id_log
-#                 sed -i '/^$/d' $cachedDB_file
-#                 last_saved_id=`tail -n1 $cachedDB_file | awk -F"," '{print $3}' | cut -f2 -d"-"`
-#                 echo "----last saved: $last_saved_id" >> $id_log
-#                 stripped_id=`echo "${last_saved_id#"${last_saved_id%%[!0]*}"}"`
-#                 echo "----stripped $stripped_id" >> $id_log
-#                 new_id=$(( stripped_id + 1 ))
-#                 first_grab="N"
-#             fi
+    # if the check is empty, add new ID
+    if [[ $check == "" ]]; then
+        echo "----assigning new ID">> $id_log
 
-#             # add zeros so the final ID is always four digits
-#             if [[ $new_id -lt 10 ]]; then
-#                 final_id="${year}ZN-000$new_id"
-#             elif [[ $new_id -lt 100 ]]; then
-#                 final_id="${year}ZN-00$new_id"
-#             elif [[ $new_id -lt 1000 ]]; then
-#                 final_id="${year}ZN-0$new_id"
-#             else
-#                 final_id="${year}ZN-$new_id"
-#             fi
+        # add zeros so the final ID is always four digits
+        if [[ $new_id -lt 10 ]]; then
+            final_id="${year}ZN-000$new_id"
+        elif [[ $new_id -lt 100 ]]; then
+            final_id="${year}ZN-00$new_id"
+        elif [[ $new_id -lt 1000 ]]; then
+            final_id="${year}ZN-0$new_id"
+        else
+            final_id="${year}ZN-$new_id"
+        fi
 
-#             # add sample with new ID to list
-#             # PROJECT_ID,OHIO_ID,WGSID,SRRID,SAMID,DATE_ASSIGNED
-#             add_line="$project_id,$sample_id,$final_id,,,$today"
-#             echo $add_line >> $cachedDB_file
-#             echo -e "$add_line" >> $id_results
-                
-#             #increase counter
-#             new_id=$(( new_id + 1 ))
-#         else
-#             echo "----sample was already assigned an ID: $check">> $id_log
-#             final_id=`echo $check |cut -f1 -d","`
-#             echo -e "$sample_id,$final_id" >> $id_results
-#         fi
-#     fi
-# done
+        #increase counter
+        new_id=$(( new_id + 1 ))
 
-# # copy the original file to the backup
-# sed -i '/^$/d' $cachedDB_file
-# cp $idDB_file $id_dir/id_db_backup.csv
+        # add sample with new ID to list
+        # PROJECT_ID,OHIO_ID,WGSID,SRRID,SAMID,DATE_ASSIGNED
+        add_line="$project_id,$sample_id,$final_id,,,$today"
+        echo $add_line >> $cachedDB_file
+    else
+        echo "----sample was already assigned an ID: $check">> $id_log
+        final_id=`echo $check |cut -f3 -d","`
+        add_line="$project_id,$sample_id,$final_id,,,$today"
+    fi
 
-# # copy the new file to master
-# cp $cachedDB_file $id_dir/id_db_master.csv
+    # add to results file
+    echo -e "$add_line" >> $id_results
+done        
 
-# # copy the cached file to the directory
-# cp $cachedDB_file $id_dir/cached
+# copy the original file to the backup
+sed -i '/^$/d' $cachedDB_file
+cat $idDB_file | uniq > $id_dir/id_db_backup.csv
+
+# copy the new file to master
+cat $cachedDB_file | uniq > $id_dir/id_db_master.csv
+
+# copy the cached file to the directory
+cp $cachedDB_file $id_dir/cached
