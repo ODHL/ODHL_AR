c39d846808054cf326002b41310f8088aa95379e
slsevilla <slsevilla@gmail.com>
2025-02-23 18:59:51 +0000

    feat: add new workflows format, report

--- run_workflow.sh
+++ run_workflow.sh
@@ -20,36 +20,47 @@ helpFunction()
 {
    echo ""
    echo "Usage: $1 -e [REQUIRED] entry"
-   echo -e "\t-e options: arBASESPACE,arANALYSIS,arPOST,outbreakANALYSIS,outbreakREPORTING,NFCORE_OUTBREAK"
+   echo -e "\t-e options: arBASESPACE,arANALYSIS,arFORMAT,arREPORT,arOUTBREAK"
    echo "Usage: $2 -i [REQUIRED] project_id"
    echo -e "\t-i project id"
-   echo "Usage: $3 -r [OPTIONAL] resume_run"
-   echo -e "\t-r Y,N option to resume a partial run settings (default Y)"
-   echo "Usage: $4 -o [OPTIONAL] outbreakReport"
-   echo -e "\t-o basic, advanced"
-   echo "Usage: $5 -n [OPTIONAL] nextflowParams"
+   echo "Usage: $3 -n [OPTIONAL] nextflowParams"
    echo -e "\t-n any nextflow configs (default --max_memory 7.GB --max_cpus 4)"
+   echo "Usage: $4 -r [OPTIONAL] resume_run"
+   echo -e "\t-r Y,N option to resume a partial run settings (default Y)"
+   echo "Usage: $5 -l [OPTIONAL] labResults"
+   echo -e "\t-l path to the labResults file"
+   echo "Usage: $6 -m [OPTIONAL] metadata_NCBI"
+   echo -e "\t-m path to the labResults file"
+   echo "Usage: $7 -o [OPTIONAL] output_NCBI"
+   echo -e "\t-o path to the labResults file"   
    exit 1 # Exit script after printing help
 }
-while getopts "e:i:r:o:n:" opt
+   # echo "Usage: $4 -o [OPTIONAL] outbreakReport"
+   # echo -e "\t-o basic, advanced"
+      # o ) outbreakReport="$OPTARG" ;;
+
+
+while getopts "e:i:n:r:l:m:o:" opt
 do
    case "$opt" in
       e ) entry="$OPTARG" ;;
       i ) project_id="$OPTARG" ;;
-      r ) resume="$OPTARG" ;;
-      o ) outbreakReport="$OPTARG" ;;
       n ) nextflowParams="$OPTARG" ;;
+      r ) resume="$OPTARG" ;;
+      l ) labResults="$OPTARG" ;;
+      m ) metadata_NCBI="$OPTARG" ;;
+      o ) output_NCBI="$OPTARG" ;;
       ? ) helpFunction ;; # Print helpFunction in case parameter is non-existent
    esac
 done
 
 # Print helpFunction in case parameters are empty
 if [ -z "$entry" ] || [ -z "$project_id" ]; then
-   echo "Some or all of the parameters are empty";
+   echo "Required -entry and/or -project_id parameters are empty";
    helpFunction
 fi
 #############################################################################################
-# args
+# Required ARGS
 #############################################################################################
 # Remove trailing / to project_name if it exists
 # some projects may have additional information (IE OH-1234 SARS ONLY) in the name
@@ -61,28 +72,57 @@ project_name=$(echo $project_id | cut -f1 -d "_" | cut -f1 -d " ")
 # set date
 today_date=$(date '+%Y-%m-%d'); today_date=`echo $today_date | sed "s/-//g"`
 
-# set optional nextflow ARGS
+# set proj dir, output dir, tmp dir
+projDir="/home/ubuntu/output/$project_name"
+outDir="$projDir/results/$entry"
+tmpDir="$projDir/tmp"
+if [[ ! -d $tmpDir ]]; then mkdir -p $tmpDir; fi
+
+#########################################################################################
+## OPTIONAL ARGS
+#########################################################################################
+# set defaults for optional nextflow ARGS
 if [ -z "$nextflowParams" ]; then 
    nextflowParams="-profile docker,test --max_memory 7.GB --max_cpus 4"
 fi
 
-# determine entry flag
-if [ $entry == "arPOST" ]; then entry="arANALYSIS --ncbiProcess TRUE"; fi
-
-#set defaults for optional resume
+# set defaults for optional resume
 if [ -z "$resume" ]; then nextflowParams="-resume $nextflowParams"; fi
 
-# set output dir, tmp dir
-outDir="/home/ubuntu/output/$project_name/results"
-if [[ ! -d $outDir/tmp ]]; then mkdir -p $outDir/tmp; fi
+# arFORMATTER settings
+if [[ $entry == "arFORMAT" ]]; then
+   nextflowParams="$nextflowParams --analysis_outdir $projDir/results/arANALYSIS"
+   
+   # set labResults, if present
+   if [ ! -z $labResults ]; then nextflowParams="$nextflowParams --labResults $labResults"; fi
 
-# arBASESPACE
+   # set metadata_NCBI, if present
+   if [ ! -z $metadata_NCBI ]; then nextflowParams="$nextflowParams --metadata_NCBI $metadata_NCBI"; fi
+
+fi
+
+# arREPORTER settings
+if [[ $entry == "arREPORT" ]]; then
+   nextflowParams="$nextflowParams --analysis_outdir $projDir/results/arANALYSIS --format_outdir $projDir/results/arFORMAT"
+
+   # set output_NCBI, if present
+   if [ ! -z $output_NCBI ]; then nextflowParams="$nextflowParams --output_NCBI $output_NCBI"; fi 
+fi
+
+
+
+#########################################################################################
+## Command
+#########################################################################################
+# Run command
 cmd="nextflow run \
    main.nf \
    $nextflowParams \
    -entry $entry \
    --outdir $outDir \
    --projectID $project_name \
-   -work-dir $outDir/tmp"
-echo $cmd
+   -work-dir $tmpDir"
+
+echo; echo "Command Running:"
+echo "$cmd"
 $cmd
